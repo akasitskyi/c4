3c7cdb59397a9cec023284d648f16552e8e4f3b5
Alex Kasitskyi <akasitskyi@users.noreply.github.com>
2018-07-28 12:43:40 +0300

    add exception

---
+++ c4/exception.hpp
@@ -0,0 +1,40 @@
+//MIT License
+//
+//Copyright(c) 2018 Alex Kasitskyi
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files(the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions :
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
+namespace c4 {
+
+	class exception : public std::runtime_error {
+	public:
+		exception(std::string msg, std::string filename, int line) : runtime_error(msg + " at " + filename + ":" + std::to_string(line)) {}
+	};
+
+#define THROW_EXCEPTION(MSG) throw c4::exception(MSG, __FILE__, __LINE__)
+
+#define ASSERT_TRUE(C) if( C ) {} else THROW_EXCEPTION("Runtime assertion failed: " #C)
+#define ASSERT_EQUAL(A, B) { auto _A = (A); auto _B = (B); if( _A != _B ) THROW_EXCEPTION("Runtime assertion failed: " #A " == " #B ", " + std::to_string(_A) + " != " + std::to_string(_B)); }
+
+};
