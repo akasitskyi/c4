8fcda71e5ad08b67858f79ca529a9cb91d5a8384
Alex Kasitskyi <a.kasitskyi@gmail.com>
2021-05-04 17:17:23 +0300

    Add pbm.hpp

---
+++ c4/pbm.hpp
@@ -0,0 +1,89 @@
+//MIT License
+//
+//Copyright(c) 2021 Alex Kasitskyi
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files(the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions :
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+#pragma once
+
+#include <vector>
+#include <cstdint>
+#include <ostream>
+#include <istream>
+#include <fstream>
+
+#include "byte_stream.hpp"
+#include "matrix.hpp"
+#include "pixel.hpp"
+
+// TODO: implement full spec
+namespace c4 {
+    namespace detail {
+        inline void get_pbm_line(std::istream& in, std::string& line) {
+            do {
+                if (!std::getline(in, line)) {
+                    THROW_EXCEPTION("PbmReader EOF");
+                }
+                line.erase(std::find(line.begin(), line.end(), '#'), line.end());
+            } while (line.empty());
+        }
+    };
+
+    inline void read_ppm(std::istream& in, matrix<pixel<uint8_t>>& out) {
+        using namespace detail;
+
+        std::string line;
+        get_pbm_line(in, line);
+        ASSERT_EQUAL(line, "P6");
+
+        get_pbm_line(in, line);
+        std::stringstream ss(line);
+        int w = -1;
+        int h = -1;
+        int maxval = -1;
+        ss >> w >> h >> maxval;
+        ASSERT_EQUAL(maxval, 255);
+
+        out.resize(h, w);
+        for(int i : range(h)) {
+            in.read((char*)out[i], w * 3);
+        }
+    }
+
+    inline void read_pgm(std::istream& in, matrix<uint8_t>& out) {
+        using namespace detail;
+
+        std::string line;
+        get_pbm_line(in, line);
+        ASSERT_EQUAL(line, "P5");
+
+        get_pbm_line(in, line);
+        std::stringstream ss(line);
+        int w = -1;
+        int h = -1;
+        int maxval = -1;
+        ss >> w >> h >> maxval;
+        ASSERT_EQUAL(maxval, 255);
+
+        out.resize(h, w);
+        for (int i : range(h)) {
+            in.read((char*)out[i], w);
+        }
+    }
+};
